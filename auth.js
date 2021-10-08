c7addd7873aeea661b1ca082427d7a0aa507a4a4
RMontepalco <montepalco.ross@gmail.com>
2021-10-08 02:42:21 -0700

    Working on Firebase Auth

--- auth.js
+++ auth.js
@@ -1,3 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyA5f0ELYbl1Wpy1rAv0B1MHVWoYT8Ot97A",
+authDomain: "resume-builder-website.firebaseapp.com",
+projectId: "resume-builder-website",
+storageBucket: "resume-builder-website.appspot.com",
+messagingSenderId: "1064855568995",
+appId: "1:1064855568995:web:b6fd5350c5e96ab665ae56",
+measurementId: "G-ZMJ23R8MF1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
 // Sign Up
 const signup = document.querySelector('#signup-form');
 signup.addEventListener('submit', (e) => {
@@ -11,7 +37,7 @@ signup.addEventListener('submit', (e) => {
 	// Sign up the user
 	console.log(email, password);
 
-	const auth = getAuth();
+	//const auth = getAuth();
 	createUserWithEmailAndPassword(auth, email, password)
 	  .then((userCredential) => {
 	    // Signed in
@@ -24,5 +50,4 @@ signup.addEventListener('submit', (e) => {
 	    const errorMessage = error.message;
 	    // ..
 	  });
-
 });
