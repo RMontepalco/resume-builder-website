e1c092be505205058a4b5fdf46758ee1a0151d15
RMontepalco <montepalco.ross@gmail.com>
2021-10-18 00:11:16 -0700

    firebase auth - logging in, logging out, and user state works

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-analytics.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,7 +24,26 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-// Sign Up
+
+// Authentication State Observer and User Data
+// Track whether a user is logged in or logged out, 
+// and show certain web elements depending on the status
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		// User is signed in, see docs for a list of available properties
+		// https://firebase.google.com/docs/reference/js/firebase.User
+		console.log('User logged in: ', user);
+		const uid = user.uid;
+		// ...
+	} else {
+		console.log('User logged out');
+		// User is signed out
+		// ...
+	}
+});
+
+
+// Create an Account (US 1, FR 1.1-1.4)
 const signup = document.querySelector('#signup-form');
 signup.addEventListener('submit', (e) => {
 	// Prevent the website from reloading when submit button is pressed.
@@ -34,16 +53,11 @@ signup.addEventListener('submit', (e) => {
 	const email = signup['signup-email'].value;
 	const password = signup['signup-password'].value;
 
-	// DELETE for later
-	console.log(email, password);
-
 	// Sign up the user
 	createUserWithEmailAndPassword(auth, email, password)
 	  .then((userCredential) => {
 	    // Store account to database
-	    console.log(userCredential);
 	    const user = userCredential.user;
-	    // ...
 	  })
 	  .catch((error) => {
 	    const errorCode = error.code;
@@ -52,4 +66,40 @@ signup.addEventListener('submit', (e) => {
 	    console.log(errorMessage);
 	    // ..
 	  });
+});
+
+
+// Log In to Account (US 2, FR 2.1-2.3)
+const login = document.querySelector('#login-form');
+login.addEventListener('submit', (e) => {
+	// Prevent the website from reloading when submit button is pressed.
+	e.preventDefault();
+
+	// Get user email and password
+	const email = login['login-email'].value;
+	const password = login['login-password'].value;
+
+	signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+		});
+});
+
+
+// Log Out of Account (US 3, FR 3.1-3.3)
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+	// Prevent the website from reloading when logout button is pressed.
+	e.preventDefault();
+
+	signOut(auth).then(() => {
+			// Sign-out successful.
+		}).catch((error) => {
+			// An error happened.
+		});
 });
