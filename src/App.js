febb8c30bf10d58d352bed0ea5ab13b92c43c3f4
RMontepalco <montepalco.ross@gmail.com>
2021-12-06 00:16:20 -0800

    reverting

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //import {Header} from './containers';
-<<<<<<< HEAD
 import {Home} from './components';
 import Login from './components/login/Login';
 import SignUp from './components/signup/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateResume from './components/createresume/CreateResume';
 import Firebase from './firebase.js';
-=======
-import {Home, Login, SignUp, Dashboard, CreateResume} from './components';
->>>>>>> 9bf9538641cae9909431246e675040657c7a5f1b
 import './App.css';
 
 const App = () => {
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,1 @@
 export { default as Home } from './home/Home';
-export { default as SignUp } from './signup/Signup';
-export { default as Login } from './login/Login';
-export { default as Dashboard } from './dashboard/Dashboard';
-export { default as CreateResume } from './createresume/CreateResume';
